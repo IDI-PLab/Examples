b0273a391e6f4a95bcc51d397ecd3d849f889121
Sitron Te <halsaune@stud.ntnu.no>
2014-12-12 15:12:01 +0100

    Analog value example now work with new bt unit
    The Arduino code is running. There might have been a point in having a
    value separator, if values are updated too often, multiple updates are
    sent as a single message.

--- AnalogValueExample/Arduino/AnalogValueExample/AnalogValueExample.ino
+++ AnalogValueExample/Arduino/AnalogValueExample/AnalogValueExample.ino
@@ -1,19 +1,15 @@
-// Inkluderinger som trengs for ADAFRUIT BLE enhet
-// Inclusions needed for ADAFRUIT BLE unit
-#include <SPI.h>
-#include "Adafruit_BLE_UART.h"
+// Inkluderer bibliotek for aa gjnnomfoere seriell kommunikasjon med bluetooth
+// Including library to do serial communication with bluetooth
+#include <SoftwareSerial.h>
 
-// Definer inn/utgangspinnene som brukes. Dette er for aa bruke ADAFRUIT sin enhet
-// Define I/O ports used. This is for ADAFRUIT unit
-#define ADAFRUITBLE_REQ 10
-#define ADAFRUITBLE_RDY 2
-#define ADAFRUITBLE_RST 9
+// Definer inn/utgangspinnene som brukes for send (TX) og motta (RX) for bluetooth
+// Define I/O ports used for transmit (TX) and receive (RX)
+const int BT_RX = 10;
+const int BT_TX = 11;
 
-Adafruit_BLE_UART BTLEserial = Adafruit_BLE_UART(ADAFRUITBLE_REQ, ADAFRUITBLE_RDY, ADAFRUITBLE_RST);
-
-// Siste status. Bruk for aa sjekke endringer
-// Last status. Use to check for differences
-aci_evt_opcode_t laststatus = ACI_EVT_DISCONNECTED;
+// Definer serieporten for kommunikasjon med bluetooth
+// Define the serial port for communication with bluetooth
+SoftwareSerial btSerial (BT_RX, BT_TX);
 
 // Siste leste verdi, for aa sjekke endringer
 // Last read value, to check for changes
@@ -23,100 +19,65 @@ int prev_val = -1;
 // Set up the unit and start advertising with BLE
 void setup(void)
 { 
+  // Start kommunikasjon med konsoll
+  // Start communication throuhg console
   Serial.begin (9600);
+  // Linja under trengs kun for Leonardo (kan slettes om du bruker Arduino UNO)
+  // The following line is only needed for Leonardo (can be deleted if you are using Arduino UNO)
   while (!Serial);
-  Serial.println ("Enkel analog verdi med ADAFRUIT");
-
-  // Navnet kan maksimalt vaere 7 bokstaver
-  // Name can max be 7 characters
-  BTLEserial.setDeviceName ("Analog");
-
-  BTLEserial.begin ();
+  Serial.println ("Enkel analog verdi med BLUETOOTH");
+  
+  // Start kommunikasjonen med bluetooth enhet
+  // Start communication with bluetooth unit
+  btSerial.begin (9600);
 }
 
 void loop()
 {
+  // Hvis vi har mottatt noe, skriv det til konsollen
+  // If we have received something, write it to console
+  if (btSerial.available () > 0) {
+    Serial.write (btSerial.read ());
+  }
+  
   // Les den nye verdien, pin 0
   // Read the new value, pin 0
   int new_val = analogRead (A0);
-  
-  // Faa enheten til aa oppdatere sed
-  // Make the unit update itself
-  BTLEserial.pollACI();
-
-  // Etterspoer status
-  // Ask for current status
-  aci_evt_opcode_t status = BTLEserial.getState();
-  
-  // Hvis statusen er endra
-  // If the status has changed
-  if (status != laststatus) {
-    // Skiv den ut
-    // print it
-    if (status == ACI_EVT_DEVICE_STARTED) {
-        Serial.println ("  Her er jeg!");
-    }
-    if (status == ACI_EVT_CONNECTED) {
-        Serial.println ("  Tilkoblet");
-    }
-    if (status == ACI_EVT_DISCONNECTED) {
-        Serial.println ("  Frakoblet");
-    }
     
-    // Husk denne statusen til neste gang
-    // Remember this status for next time
-    laststatus = status;
-  }
-
-  // Hvis vi er tilkoblet
-  // If we are connected
-  if (status == ACI_EVT_CONNECTED) {
+  // Vi kan naa sjekke om verdien har endra seg, og hvis den har det send den. Ha litt feilmarginer
+  // We can now check if th value has changed, and if so send it. Have some morgins
+  int diff = new_val - prev_val;
+  if (abs(diff) > 5) {
+    // Foerst trenger vi ett sted aa lagre teksten som skal sendes
+    // First we need a place to store the text to send
+    // Denne maaten garanterer vi har stort no rom til aa lagre verdien uansett hvilket tall det er
+    // This way guarantees we have enough space no matter the number
+    // Vi kunne brukt 5 siden vi vet selve tallet er mellom 0 og 1024 (4 tegn) og ett tegn til avslutningsbokstanven
+    // We could have used 5 since we know the number is between 0 and 1024 (4 characters) and one character to the end character
+    int txt_size = sizeof (int) * 3 + 1;
+    char text[txt_size];
+    // Det finnes alternativer til itoa, men den viker som den skal
+    // The are alternatives to itoa, but it works as it should
+    // 10 til slutt betyr vi faar tallet i desimal format (slik som vi bruker til vanlig)
+    // 10 in the end means vi get the number in decimal format (which we normally use)
+    itoa (new_val, text, 10);
     
-    // Hvis vi mottok noe, kan vi printe det. Vi skulle egentlig ikke faa noe.
-    // If we reveived something, we can print it. We should not get anything here though
-    if (BTLEserial.available ()) {
-      while (BTLEserial.available ()) {
-        Serial.print (BTLEserial.read ());
-      }
+    // sjekk den faktiske stoerrelsen paa teksten
+    // Check the actual size of the text
+    int used_size = 0;
+    while ((used_size < txt_size ) && (text[used_size] != 0)) {
+      used_size++;
     }
     
+    // Send data
+    // Send data
+    // Vi trenger et annet format naar vi sender (dette er pga hvordan biblioteket vi bruker definerer send data) -> casting
+    // We need another format when we send (due to how the library we use define send data) -> casting
+    //BTLEserial.write (send_buffer, used_size);
+    btSerial.write ((uint8_t*)text, used_size);
     
-    // Vi kan naa sjekke om verdien har endra seg, og hvis den har det send den. Ha litt feilmarginer
-    // We can now check if th value has changed, and if so send it. Have some morgins
-    int diff = new_val - prev_val;
-    if (abs(diff) > 5) {
-      // Foerst trenger vi ett sted aa lagre teksten som skal sendes
-      // First we need a place to store the text to send
-      // Denne maaten garanterer vi har stort no rom til aa lagre verdien uansett hvilket tall det er
-      // This way guarantees we have enough space no matter the number
-      // Vi kunne brukt 5 siden vi vet selve tallet er mellom 0 og 1024 (4 tegn) og ett tegn til avslutningsbokstanven
-      // We could have used 5 since we know the number is between 0 and 1024 (4 characters) and one character to the end character
-      int txt_size = sizeof (int) * 3 + 1;
-      char text[txt_size];
-      // Det finnes alternativer til itoa, men den viker som den skal
-      // The are alternatives to itoa, but it works as it should
-      // 10 til slutt betyr vi faar tallet i desimal format (slik som vi bruker til vanlig)
-      // 10 in the end means vi get the number in decimal format (which we normally use)
-      itoa (new_val, text, 10);
-      
-      // sjekk den faktiske stoerrelsen paa teksten
-      // Check the actual size of the text
-      int used_size = 0;
-      while ((used_size < txt_size ) && (text[used_size] != 0)) {
-        used_size++;
-      }
-      
-      // Send data
-      // Send data
-      // Vi trenger et annet format naar vi sender (dette er pga hvordan biblioteket vi bruker definerer send data) -> casting
-      // We need another format when we send (due to how the library we use define send data) -> casting
-      //BTLEserial.write (send_buffer, used_size);
-      BTLEserial.write ((uint8_t*)text, used_size);
-      
-      // Husk aa oppdatere forrige verdi
-      // Remember to update the last value
-      prev_val = new_val;
-    }
-    
+    // Husk aa oppdatere forrige verdi
+    // Remember to update the last value
+    prev_val = new_val;
   }
 }
